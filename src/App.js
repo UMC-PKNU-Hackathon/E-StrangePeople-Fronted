8dd12122f33e44297e10b283711802ed181ed53b
aong13 <oaoa13@pukyong.ac.kr>
2023-11-25 03:52:55 +0900

    commit 3:52

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Search from "./pages/search";
 import SearchResult from "./pages/searchResult";
 import Post from "./components/post/Post";
 import Comment from "./components/post/Comment";
+import ReadPost from "./components/post/ReadPost";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/search" element={<Search />} />
           <Route path="/search/:query" element={<SearchResult />} />
           <Route path="/post" element={<Post />} />
+          <Route path="/post-1" element={<ReadPost />} />
         </Routes>
       </BrowserRouter>
     </div>
